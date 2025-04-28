4a7a2f3b2cc8d8a0e34b588230fdeb5388766ae0
k-rachelle <62003156+k-rachelle@users.noreply.github.com>
2025-04-27 17:17:21 -0700

    Forecasting two QoI

--- Code/Simulations/Forecasting.py
+++ Code/Simulations/Forecasting.py
@@ -20,6 +20,7 @@ t = Forecasting_Model.t
 S0 = Forecasting_Model.S0
 my_SIR_solutions = Forecasting_Model.my_SIR_solutions
 my_SIR_model = Forecasting_Model.my_SIR_model
+my_SIR_model_2D = Forecasting_Model.my_SIR_model_2D
 
 random.seed(3)
 np.random.seed(3)
@@ -235,6 +236,144 @@ def forecastSingleQoI(T_initial, QoI_initial, T_final, QoI_final, pred_q_linspac
     plotPredictedQoIRegion(q_final, pred_q_linspace, pred_q_final_KDE, l1, l2, T_final, QoI_lab = QoI_lab)
 
 
+def forecastTwoQoI(firstT_initial, firstQoI_initial, secondT_initial, secondQoI_initial, T_final, QoI_final, pred_q_linspace=None, l1=None, l2=None):
+    
+    # # Time of prediction
+    # T_final = 60
+    # QoI_final = "i"
+    
+    # Set Initial Conditions
+    T0 = 10
+    
+    num_samples = 10000
+    num_samples_obs = 10000
+    num_resamples = 10000
+    
+    QoI_lab = "[" + firstQoI_initial + "(" + str(firstT_initial) + "), " + secondQoI_initial + "(" + str(secondT_initial) + ")] to " + QoI_final + "(" + str(T_final) + ")"
+
+    # Set "true" domain to generate data; try to reobtain domain
+    random.seed(3)
+    np.random.seed(3)
+    # -----------------------------------
+    # Beta = infection rate (parameter 0)
+    # Gamma = recovery rate (parameter 1)
+    # -----------------------------------
+    
+    beta1_true = 0
+    beta2_true = 1
+    gamma1_true = 0
+    gamma2_true = 1
+    
+    
+    lambda_true = np.array([[beta1_true, beta2_true],
+                            [gamma1_true, gamma2_true]])
+    
+    
+    # -----------------------------------------------------------------------------
+    # Generate observed data ------------------------------------------------------
+    # -----------------------------------------------------------------------------
+    
+    sampler_obs = bsam.sampler(my_SIR_model_2D) # Sampler for "true" QoI
+    input_samples_obs = samp.sample_set(2)
+    input_samples_obs.set_domain(lambda_true)
+    
+    # Generate samples on the parameter space
+    
+    # Data-generating beta distributions
+    input_samples_obs = sampler_obs.random_sample_set(
+        [['beta', {'a': 12, 'b': 30}], 
+            ['beta', {'a': 6, 'b': 30}]], 
+        input_samples_obs, num_samples=num_samples_obs)
+    
+    s_mat, i_mat, r_mat = my_SIR_solutions(input_samples_obs.get_values())
+    
+    
+    # -------------------------------------------------------------------------
+    # Get Initial and Final QoI------------------------------------------------
+    # -------------------------------------------------------------------------
+    if firstQoI_initial == "s":
+        firstq_mat_initial = 1-s_mat
+    elif firstQoI_initial == "i":
+        firstq_mat_initial = i_mat
+        
+    if secondQoI_initial == "s":
+        secondq_mat_initial = 1-s_mat
+    elif secondQoI_initial == "i":
+        secondq_mat_initial = i_mat
+        
+    if QoI_final == "s":
+        q_mat_final = 1-s_mat
+    elif QoI_final == "i":
+        q_mat_final = i_mat
+    
+    firstq_initial = (firstq_mat_initial[:, T0 + firstT_initial - 1] - firstq_mat_initial[:, T0 - 1])/firstT_initial # "Observed" data at initial time; use to solve inverse
+    secondq_initial = (secondq_mat_initial[:, T0 + secondT_initial - 1] - secondq_mat_initial[:, T0 - 1])/secondT_initial # "Observed" data at initial time; use to solve inverse
+    
+    q_final = (q_mat_final[:, T0 + T_final - 1] - q_mat_final[:, T0 - 1])/T_final # "Observed" data at final time; use to evaluate predictions
+    
+    output_samples_obs = samp.sample_set(2)   # Changed to (2) instead of (1) for 2D QoI output
+    output_samples_obs.set_values([firstq_initial, secondq_initial])
+    
+    # -------------------------------------------------------------------------
+    # Sample parameter domain uniformly, compute QoIs -------------------------
+    # -------------------------------------------------------------------------
+    
+    sampler = bsam.sampler(my_SIR_model_2D)
+    
+    
+    # Initialize 2-dimensional input parameter sample set object
+    input_samples = samp.sample_set(2)
+    
+    beta1 = 0.15
+    beta2 = 0.45
+    gamma1 = 0.05
+    gamma2 = 0.3
+    
+        
+    # Set parameter domain - Determine reasonable values for beta and gamma
+    input_samples.set_domain(np.array([[beta1, beta2],    # beta
+                                       [gamma1, gamma2]])) # gamma
+    
+    
+    # Generate uniform samples on the parameter space
+    input_samples = sampler.random_sample_set('uniform', input_samples, num_samples=num_samples)
+    
+    output_samples_values = my_SIR_model_2D(input_samples.get_values(), T0 = T0,  firstT = firstT_initial, secondT = secondT_initial, Q1 = firstQoI_initial, Q2 = secondQoI_initial)
+    output_samples = samp.sample_set(2)
+    output_samples.set_values(output_samples_values)
+    
+    # Create the prediction discretization object using the input samples
+    disc_predict = samp.discretization(input_samples, output_samples)
+    
+    # # Set probability set for predictions
+    disc_predict.set_output_observed_set(output_samples_obs)
+    
+    
+    # Inversion
+    calculateR.invert_to_kde(disc_predict)
+    
+       
+    input_sample_values = input_samples.get_values() 
+    weights = input_samples.get_weights()
+    
+    # -------------------------------------------------------------------------
+    # Resample from solution --------------------------------------------------
+    # -------------------------------------------------------------------------
+    
+    from scipy.stats import gaussian_kde
+    
+    joint_kde = gaussian_kde(input_sample_values.T, weights = weights)
+    
+    new_samp = joint_kde.resample(num_resamples).T
+    # beta_samp = new_samp[:, 0]
+    # gamma_samp = new_samp[:, 1]
+    
+    # Predict to final time
+    pred_q_final = my_SIR_model(new_samp, T0, T_final, QoI_final)
+    pred_q_final_KDE = gaussian_kde(pred_q_final)
+    plotPredictedQoIRegion(q_final, pred_q_linspace, pred_q_final_KDE, l1, l2, T_final,  QoI_lab = QoI_lab)
+
+# Forecasts from single-QoI inversions
 forecastSingleQoI(T_initial = 10, QoI_initial = "s", T_final = 30, QoI_final = "s", 
                   pred_q_linspace = np.linspace(-0.005, 0.035, 5000) , l1 = -0.005, l2 = 0.02925) # l1 and l2 determined to represent a 95% probability region using findHighProbRegion()
 forecastSingleQoI(T_initial = 10, QoI_initial = "s", T_final = 60, QoI_final = "s",
@@ -254,3 +393,12 @@ forecastSingleQoI(T_initial = 30, QoI_initial = "s", T_final = 30, QoI_final = "
 forecastSingleQoI(T_initial = 30, QoI_initial = "s", T_final = 60, QoI_final = "i",
                     pred_q_linspace = np.linspace(-0.001, 0.006, 5000), l1 = -0.00045927,  l2 = 0.00270688)
 
+# Forecasts from two-QoI inversions
+forecastTwoQoI(firstT_initial = 30, firstQoI_initial = "s", secondT_initial= 30, secondQoI_initial = "i", T_final = 60, QoI_final = "s",
+                pred_q_linspace = np.linspace(-0.005, 0.02, 5000), l1 = -0.00124319,  l2 = 0.01673326)
+forecastTwoQoI(firstT_initial = 30, firstQoI_initial = "s", secondT_initial= 30, secondQoI_initial = "i", T_final = 60, QoI_final = "i",
+                pred_q_linspace = np.linspace(-0.001, 0.005, 5000), l1 = -0.00037495,  l2 = 0.01684886)
+forecastTwoQoI(firstT_initial = 30, firstQoI_initial = "s", secondT_initial= 31, secondQoI_initial = "s", T_final = 60, QoI_final = "s",
+                pred_q_linspace = np.linspace(-0.003, 0.02, 5000), l1 = -0.00101035,  l2 = 0.01674927)
+forecastTwoQoI(firstT_initial = 30, firstQoI_initial = "s", secondT_initial= 31, secondQoI_initial = "s", T_final = 60, QoI_final = "i",
+                pred_q_linspace = np.linspace(-0.001, 0.006, 5000), l1 = -0.0004467,  l2 = 0.00278554)
--- Code/Simulations/Forecasting_Model.py
+++ Code/Simulations/Forecasting_Model.py
@@ -100,3 +100,47 @@ def my_SIR_model(parameter_samples, T0, T, Q = "s"):
                 QoI[i] = (R[T1] - R[T0])/T
         return QoI
 
+
+
+# 2D QoI map
+def my_SIR_model_2D(parameter_samples, T0, firstT, secondT, Q1 = "s", Q2 = "s"):
+    # Output 2D QoI; using two time points or two QoI
+        firstT1 = T0 + firstT 
+        secondT1 = T0 + secondT
+        if parameter_samples.shape == (2,):
+            beta = parameter_samples[0]
+            gamma = parameter_samples[1]
+        else:
+            beta = parameter_samples[:, 0]
+            gamma = parameter_samples[:, 1]
+            
+        QoI1 = np.empty(parameter_samples.shape[0])
+        QoI2 = np.empty(parameter_samples.shape[0])
+        
+        s_mat = np.empty([parameter_samples.shape[0], num_days])
+        i_mat = np.empty([parameter_samples.shape[0], num_days])
+        r_mat = np.empty([parameter_samples.shape[0], num_days])
+        
+        for i in range(parameter_samples.shape[0]):
+            sol = odeint(deriv, y0, t, args=(beta[i], gamma[i]))
+            S, I, R = sol.T
+
+            s_mat[i] = S
+            i_mat[i] = I
+            r_mat[i] = R
+            
+            
+            if Q1 == "s":   
+                QoI1[i] = (S[T0-1] - S[firstT1-1])/firstT
+            elif Q1 == "i":
+                QoI1[i] = (I[firstT1] - I[T0])/firstT
+            elif Q1 == "r":
+                QoI1[i] = (R[firstT1] - R[T0])/firstT
+                            
+            if Q2 == "s":   
+                QoI2[i] = (S[T0-1] - S[secondT1-1])/secondT
+            elif Q2 == "i":
+                QoI2[i] = (I[secondT1] - I[T0])/secondT
+            elif Q1 == "r":
+                QoI2[i] = (R[secondT1] - R[T0])/secondT
+        return QoI1, QoI2
